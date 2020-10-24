7624e9250ad10f9f5f9893be29ede0fa1f63786a
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2020-10-24 07:57:39 +0200

    Create edgar gilbert parameters

--- __main__.py
+++ __main__.py
@@ -1,2 +1,15 @@
+import sys
+from graph import Graph
+
+DATA_DIR = "data/"
+
+def create_edgar_gilbert_graph():
+    graph = Graph.edgar_gilbert_graph(1000)
+    graph.write_parameters(DATA_DIR + "edgar_gilbert_graph.csv")
+
+
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "eg":
+            create_edgar_gilbert_graph()
+
