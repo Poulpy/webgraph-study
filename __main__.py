4af78fdaaeadc5c88aa868187b6cba4d30852b0d
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2020-10-24 08:12:05 +0200

    Barabasi albert parameters weird ?

--- __main__.py
+++ __main__.py
@@ -7,9 +7,14 @@ def create_edgar_gilbert_graph():
     graph = Graph.edgar_gilbert_graph(1000)
     graph.write_parameters(DATA_DIR + "edgar_gilbert_graph.csv")
 
+def create_barabasi_albert_graph():
+    graph = Graph.barabasi_albert_graph(1000)
+    graph.write_parameters(DATA_DIR + "barabasi_albert_graph.csv")
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1] == "eg":
             create_edgar_gilbert_graph()
+        elif sys.argv[1] == "ba":
+            create_barabasi_albert_graph()
 
